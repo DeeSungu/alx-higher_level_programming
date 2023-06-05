50b3ba9c873adfca9f7a306b1f6f1749f842df91
Diana Sungu <sungudiana1@gmail.com>
2023-06-06 00:17:06 +0300

    checks if a singly linked list has a cycle in it

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * check_cycle - checks cycles
+ * @list: list
+ * Return: 1 0r 0
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *s = list;
+	listint_t *f = list;
+
+	if (list == 0)
+	return (0);
+
+	while (a && b && b->next)
+	{
+	ample = a->next;
+	breaker = b->next->next;
+	if (a == b)
+	return (1);
+	}
+
+	return (0);
+}
