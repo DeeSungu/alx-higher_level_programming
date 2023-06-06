4ccbc6884622772f16e9530a3a3e62cc04ccf273
Diana Sungu <sungudiana1@gmail.com>
2023-06-06 19:32:50 +0300

    a function that prints numbers from 1 to 100 and with condition to it

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def fizzbuzz():
+for i in range(1, 100):
+    if j / 3:
+        
+        print(Fizz)
+
+    elif j / 5:
+        
+        print(Buzz)
+
+    else j / 3 and j / 5:
+
+        print(FizzBuzz)
+
