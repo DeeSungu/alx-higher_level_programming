1c5208171e397532872130691ba07232327a9003
Diana Sungu <sungudiana1@gmail.com>
2023-06-08 19:58:49 +0300

    a program that imports functions from a file and does some moth and prints results

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the modulo, multiplication, quotient ans subtraction of 10 and 5."""
+    from calculator_1 import mod, mul, div, sub
+
+    a = 10
+    b = 5
+
+    print("{} % {} = {}".format(a, b, mod(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+
