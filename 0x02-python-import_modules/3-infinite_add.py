a5d589c1cf8e88e1c315b652ec35831c24cd76e0
Diana Sungu <sungudiana1@gmail.com>
2023-06-08 20:47:00 +0300

    a program that outputs the results from the addition of arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """outputs the results from the addition of all arguments."""
+    """prints the addition of all arguments."""
     import sys
 
     total = 0
