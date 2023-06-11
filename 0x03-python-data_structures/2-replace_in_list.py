bf0f324beff827524a8eefd15740b1434e99a6b8
Diana Sungu <sungudiana1@gmail.com>
2023-06-11 18:47:08 +0300

    a function that replaces elements of a list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    """Replace elements of a list"""
+    if not (0 <= idx < len(my_list)):
+        return my_list
+    my_list[idx] = element
+    return my_list
