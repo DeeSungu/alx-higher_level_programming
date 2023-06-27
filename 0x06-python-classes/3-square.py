9cadb8bb540f6a1c9edf335f54bbad4d65239663
Diana Sungu <sungudiana1@gmail.com>
2023-06-27 18:17:23 +0300

    A class square that defines a square with a private intance attribute and a public instance method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Based on class square"""
+
+
+class Square:
+    """Defines the square"""
+
+    def __init__(self, size=0):
+        """instance"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Area"""
+        return (self.__size * self.__size)
