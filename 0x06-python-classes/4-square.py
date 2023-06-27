05044ff86ee0854b2568a41920d4f2b108d2fda9
Diana Sungu <sungudiana1@gmail.com>
2023-06-27 18:21:10 +0300

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""based on class square"""
+
+
+class Square:
+    """defines the class square"""
+
+    def __init__(self, size=0):
+        """instance"""
+        self.size = size
+
+    @property
+    def size(self):
+        """self"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Area"""
+        return (self.__size * self.__size)
