59043b6d89e8da7cc10e3b3065980d9cb85d82ee
Diana Sungu <sungudiana1@gmail.com>
2023-09-11 23:58:17 +0300

    A script that prints 3 different lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const linec = 'C is fun';
-const lineb = 'Python is cool';
-const linea = 'Javascript is amazing';
-console.log(`${linec}\n${lineb}\n${linea}`);
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'Javascript is amazing';
+console.log(`${line1}\n${line2}\n${line3}`);
