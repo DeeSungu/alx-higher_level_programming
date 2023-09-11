c8e88d9778230ea75c09498cc62959b52c0b2609
Diana Sungu <sungudiana1@gmail.com>
2023-09-12 01:03:56 +0300

    A script that prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// A script that adds first and second args, prints result
-const a = Math.floor(Number(process.argv[3]));
-const b = Math.floor(Number(process.argv[4]));
-console.log(a + b);
+function add (a, b) {
+  return (a + b);
+}
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
