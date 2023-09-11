0ab73fe11654aa355cc8c971f83507225d8df9cf
Diana Sungu <sungudiana1@gmail.com>
2023-09-12 01:01:54 +0300

    A script that prints 3 lines but using an array of stringand a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-//A script that prints a loop through 3 messages to print to console
-const msgArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < 3; i++) {
-  console.log(msgArray[i]);
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const line in lines) {
+  console.log(lines[line]);
 }
