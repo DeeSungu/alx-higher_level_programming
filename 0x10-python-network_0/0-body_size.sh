36993a51ef74d7e1752caf0c3171298c31e2dcd2
Diana Sungu <sungudiana1@gmail.com>
2023-09-28 18:09:29 +0300

    A bash script that displays the size of the body of the response of a curl request

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #A bash script that takes in a URL, sends request
 #and displays size ofthe body response.
-curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
+curl -so /dev/null -w '%{size_download}\n' "$1"
